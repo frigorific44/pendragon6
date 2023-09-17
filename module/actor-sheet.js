ae571acbe8068bfd75104d7dc6a473161ca3a7be
Derek Enlow <theunnamed97@gmail.com>
2023-09-16 18:57:27 -0600

    passion controls

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -28,10 +28,35 @@ export class PendragonActorSheet extends ActorSheet {
       secrets: this.document.isOwner,
       async: true
     });
+
+    // Prepare items
+    this._prepareItems(context);
+
     return context;
   }
 
   /* -------------------------------------------- */
+
+  /**
+   * Organize and classify Items for Character sheets.
+   * @param {Object} actorData The actor to prepare.
+   */
+  _prepareItems(sheetData) {
+    // Initialize containters.
+    const inventory = {};
+
+    for (let i of sheetData.items) {
+      let item = i;
+      i.img = i.img || DEFAULT_TOKEN;
+
+      if (!inventory[i.type]) {
+        inventory[i.type] = [];
+      }
+      inventory[i.type].push(i);
+    }
+
+    sheetData.inventory = inventory;
+  }
 
   /** @inheritdoc */
   activateListeners(html) {
@@ -39,6 +64,37 @@ export class PendragonActorSheet extends ActorSheet {
 
     html.find(".traits").on("click", ".rollable", this._onTraitRoll.bind(this));
     html.find(".traits").on("click", ".trait-plus", this._onTraitAdd.bind(this));
+
+    // Render the item sheet for viewing/editing prior to the editable check.
+    html.find('.item-edit').on("click", ev => {
+      const id = ev.currentTarget.dataset.itemId;
+      const item = this.actor.items.get(id);
+      item.sheet.render(true);
+    });
+
+    // Checkbox toggle
+    html.find('.item-checkbox').on("click", ev => {
+      ev.preventDefault();
+      const itemId = ev.currentTarget.dataset.itemId;
+      const item = this.actor.items.get(itemId);
+      let toggle = !item.system.isChecked;
+      const updateData = {
+        "system.isChecked": toggle
+      };
+      item.update(updateData);
+    });
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.isEditable) return;
+
+    // Add item
+    html.find('.item-create').on("click", this._onItemCreate.bind(this));
+
+    // Delete item
+    html.find('.item-delete').on("click", ev => {
+      const itemId = ev.currentTarget.dataset.itemId;
+      this.actor.deleteEmbeddedDocuments("Item",[itemId]);
+    });
   }
 
   /* -------------------------------------------- */
@@ -75,6 +131,21 @@ export class PendragonActorSheet extends ActorSheet {
     if (dataset.dual && dataset.trait) {
       this.actor.shiftOneTowardsTrait(dataset.dual, dataset.trait);
     }
+  }
+
+  async _onItemCreate(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const type = element.dataset.type;
+    const name = game.i18n.localize(`PENDRAGON.New${type.capitalize()}Name`)
+    const itemData = {
+      name: name,
+      type: type,
+      data: duplicate(element.dataset)
+    };
+
+    delete itemData.data["type"];
+    return this.actor.createEmbeddedDocuments("Item", [itemData]);
   }
 
 }
