ee54c4ae8c7c50c6d1d3abf66d617da553ce8fa2
Derek Enlow <theunnamed97@gmail.com>
2023-09-21 02:53:17 -0600

    converted remaining previous fields to DataModel

--- module/models/character.js
+++ module/models/character.js
@@ -14,6 +14,18 @@ export class CharacterData extends foundry.abstract.DataModel {
                     required: true, nullable: false, integer: true, min: 0, initial: 0
                 })
             }), {initialKeys: CONFIG.PENDRAGON.characteristics, initialKeysOnly: true}),
+            traits: new foundry.data.fields.SchemaField({
+                ...CONFIG.PENDRAGON.traits.map((dual) => {
+                    return new MappingField( new foundry.data.fields.SchemaField({
+                        value: new foundry.data.fields.NumberField({
+                            required: true, nullable: false, integer: true, min: 0, initial: 10
+                        }),
+                        isChecked: new foundry.data.fields.BooleanField({
+                            required: true, nullable: false, initial: false
+                        })
+                    }), {initialKeys: dual, initialKeysOnly: true});
+                })
+            }),
             skills: new foundry.data.fields.SchemaField({
                 combat: new MappingField(new foundry.data.fields.SchemaField({
                     value: new foundry.data.fields.NumberField({
