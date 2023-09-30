3aee256a75378c5de3675d0a309da713ea9e9ad7
Derek Enlow <theunnamed97@gmail.com>
2023-09-30 17:00:46 -0600

    removed field object path boilerplate

--- module/models/character.js
+++ module/models/character.js
@@ -3,54 +3,55 @@ import { MappingField } from "./fields.js"
 
 export class CharacterData extends foundry.abstract.DataModel {
     static defineSchema() {
+        const { BooleanField, NumberField, SchemaField, StringField } = foundry.data.fields;
         return {
-            personal: new MappingField(new foundry.data.fields.SchemaField({
-                value: new foundry.data.fields.StringField({
+            personal: new MappingField(new SchemaField({
+                value: new StringField({
                     required: true, nullable: false, initial: ""
                 })
             }), {initialKeys: CONFIG.PENDRAGON.personal, initialKeysOnly: true}),
-            characteristics: new MappingField(new foundry.data.fields.SchemaField({
-                value: new foundry.data.fields.NumberField({
+            characteristics: new MappingField(new SchemaField({
+                value: new NumberField({
                     required: true, nullable: false, integer: true, min: 0, initial: 0
                 })
             }), {initialKeys: CONFIG.PENDRAGON.characteristics, initialKeysOnly: true}),
-            traits: new foundry.data.fields.SchemaField({
+            traits: new SchemaField({
                 ...CONFIG.PENDRAGON.traits.map((dual) => {
-                    return new MappingField( new foundry.data.fields.SchemaField({
-                        value: new foundry.data.fields.NumberField({
+                    return new MappingField( new SchemaField({
+                        value: new NumberField({
                             required: true, nullable: false, integer: true, min: 0, initial: 10
                         }),
-                        isChecked: new foundry.data.fields.BooleanField({
+                        isChecked: new BooleanField({
                             required: true, nullable: false, initial: false
                         })
                     }), {initialKeys: dual, initialKeysOnly: true});
                 })
             }),
-            skills: new foundry.data.fields.SchemaField({
-                combat: new MappingField(new foundry.data.fields.SchemaField({
-                    value: new foundry.data.fields.NumberField({
+            skills: new SchemaField({
+                combat: new MappingField(new SchemaField({
+                    value: new NumberField({
                         required: true, nullable: false, integer: true, min: 0, initial: 0
                     }),
-                    isChecked: new foundry.data.fields.BooleanField({
+                    isChecked: new BooleanField({
                         required: true, nullable: false, initial: false
                     })
                 }), {initialKeys: CONFIG.PENDRAGON.combat_skills, initialKeysOnly: true}),
-                general: new MappingField(new foundry.data.fields.SchemaField({
-                    value: new foundry.data.fields.NumberField({
+                general: new MappingField(new SchemaField({
+                    value: new NumberField({
                         required: true, nullable: false, integer: true, min: 0, initial: 0
                     }),
-                    isChecked: new foundry.data.fields.BooleanField({
+                    isChecked: new BooleanField({
                         required: true, nullable: false, initial: false
                     })
                 }), {initialKeys: CONFIG.PENDRAGON.general_skills, initialKeysOnly: true})
             }),
-            wounds: new foundry.data.fields.SchemaField({
+            wounds: new SchemaField({
                 ...Array(CONFIG.PENDRAGON.maxWounds).fill(0).map((x) =>
-                    new foundry.data.fields.SchemaField({
-                        damage: new foundry.data.fields.NumberField({
+                    new SchemaField({
+                        damage: new NumberField({
                             required: true, nullable: false, integer: true, initial: 0
                         }),
-                        description: new foundry.data.fields.StringField({
+                        description: new StringField({
                             required: true, nullable: false, initial: ""
                         })
                     })
