f997c1474e5bb93feb9fca463803afd6a39b87af
Derek Enlow <theunnamed97@gmail.com>
2023-10-17 22:48:39 -0600

    added roll dialogue

---
+++ module/roll-dialogue.js
@@ -0,0 +1,57 @@
+export default class RollDialogue extends Application {
+    constructor(actor, category, label, value, ...args) {
+        super(...args);
+        this.actor = actor;
+        this.category = category;
+        this.label = label;
+        this.value = value;
+    }
+
+    /**
+     * Extend and override the default options used by the 5e Actor Sheet
+     * @returns {Object}
+     */ 
+	static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["pendragon", "dialogue"],
+      template: "systems/pendragon6/templates/dialogues/roll-dialogue.hbs",
+      resizable: false,
+      title: game.i18n.localize("PENDRAGON.RollOptions")
+    });
+  }
+
+  /** @inheritdoc */
+  getData() {
+    // return {"parts": this.actor.system.armor.parts};
+    return {
+        category: this.category,
+        label: this.label,
+        value: this.value
+    };
+  }
+
+  /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // html.find('input').change(ev => this._onChangePart(ev));
+  }
+
+//   async _onChangePart(event) {
+//     const partKey = $(event.currentTarget).closest("div").data().part;
+//     const partField = event.currentTarget.dataset.field;
+//     let fieldValue = event.currentTarget.value;
+//     if (partField === "worn") {
+//       fieldValue = $(event.currentTarget).prop("checked");
+//     }
+//     let parts = duplicate(this.actor.system.armor.parts);
+//     parts[partKey][partField] = fieldValue;
+
+//     let updateData = {};
+//     updateData[`system.armor.parts.${partKey}.${partField}`] = fieldValue;
+//     await this.actor.update(updateData);
+
+//     this.render();
+//   }
+  
+}
