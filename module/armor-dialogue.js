823747ee31fca74289e0319e09d6bc05580bae42
Derek Enlow <theunnamed97@gmail.com>
2023-10-18 22:20:46 -0600

    changed parentage to FormApplication

--- module/armor-dialogue.js
+++ module/armor-dialogue.js
@@ -1,5 +1,5 @@
 /** Class for a dialogue to edit a sheet's armor parts. */
-export default class ArmorDialogue extends Application {
+export default class ArmorDialogue extends FormApplication {
     constructor(actor, ...args) {
         super(...args);
         this.actor = actor;
@@ -14,6 +14,9 @@ export default class ArmorDialogue extends Application {
       classes: ["pendragon", "dialogue"],
       template: "systems/pendragon6/templates/dialogues/armor-dialogue.hbs",
       resizable: false,
+      closeOnSubmit: false,
+      submitOnClose: true,
+      submitOnChange: true,
       title: game.i18n.localize("PENDRAGON.ArmorComponents")
     });
   }
@@ -22,32 +25,10 @@ export default class ArmorDialogue extends Application {
   getData() {
     return {"parts": this.actor.system.armor.parts};
   }
-
-  /** @inheritdoc */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    html.find('input').on("change", ev => this._onChangePart(ev));
-  }
-
-  async _onChangePart(event) {
-    const partKey = $(event.currentTarget).closest("div").data().part;
-    const partField = event.currentTarget.dataset.field;
-    let fieldValue = event.currentTarget.value;
-    if (partField === "worn") {
-      fieldValue = $(event.currentTarget).prop("checked");
-    }
-    let parts = duplicate(this.actor.system.armor.parts);
-    parts[partKey][partField] = fieldValue;
-
-    let updateData = {};
-    updateData[`system.armor.parts.${partKey}.${partField}`] = fieldValue;
-    await this.actor.update(updateData);
-  }
-
-  /** @override */
-  async close(options) {
-    await super.close(options);
+  
+  async _updateObject(event, formData) {
+    console.log(formData);
+    await this.actor.update(formData);
   }
 
 }
